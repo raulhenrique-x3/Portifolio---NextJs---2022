34b7c7e8593e7c7807b3744cc6fa45f2db7e500f
Raul Henrique <raulheri43@hotmail.com>
2022-10-01 01:08:19 -0300

    melhorias no seo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  distDir: "build",
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+    });
+    return config;
+  },
+};
