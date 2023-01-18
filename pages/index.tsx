b75926cff32f3f1f429d9854795d178e0223ff66
Raul Henrique <raulheri43@hotmail.com>
2023-01-17 23:05:14 -0300

    aperfeiçoamento do seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,16 @@ const Home: NextPage = () => {
             content="Estudante de Análise e Desenvolvimento de Sistemas no Instituto Federal de Ciência e Tecnologia de Pernambuco, (IFPE), tenho 22 anos, e sou um Desenvolvedor Front-End, bastante dedicado, anti-frágil, criativo e, com muito amor pelo que faço!, Vamos bater um papo?"
           />
           <meta name="google-site-verification" content="xRK8Fmlx2OrV0hN-K7k9lpxVBnamDLzWrUUXu0wPNxQ" />
+          <meta property="og:site_name" content="Portifólio do Desenvolvedor Front-End Raul Henrique"></meta>
+          <meta
+            property="og:image"
+            content="https://raul-henrique-dev.vercel.app/_next/image?url=%2FlogoRH.png&w=128&q=75"
+          ></meta>
+          <meta property="og:image:width" content="128"></meta>
+          <meta property="og:image:height" content="75"></meta>
+          <meta property="og:image:type" content="image/png"></meta>
+          <link rel="dns-prefetch" href="//raul-henrique-dev.vercel.app"></link>
+          <link rel="dns-prefetch" href="//fonts.googleapis.com"></link>
           <link rel="icon" type="image/x-icon" href="/logotitle.png" />
           <script
             type="application/ld+json"
